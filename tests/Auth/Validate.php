ca433dc8caccc1772d93583920c046aa8c0c926a
Andreas Ek <andreas@elseif.se>
2019-06-06 22:39:53 +0200

    Extend pno tests

--- tests/Auth/Validate.php
+++ tests/Auth/Validate.php
@@ -4,12 +4,28 @@ namespace ElseifAB\Tests\IDKollen\Auth;
 
 class Validate extends \WP_UnitTestCase
 {
-    public function testSocialSecurityNumber()
+    public function testEmptySocialSecurityNumber()
     {
         $this->assertFalse(\ElseifAB\IDKollen\Auth\Validate::socialSecurityNumber(''));
+    }
+
+    public function testShortSocialSecurityNumber()
+    {
         $this->assertFalse(\ElseifAB\IDKollen\Auth\Validate::socialSecurityNumber('123'));
+    }
+
+    public function testInvalidSocialSecurityNumber()
+    {
         $this->assertFalse(\ElseifAB\IDKollen\Auth\Validate::socialSecurityNumber('8112189877'));
+    }
+
+    public function testSocialSecurityNumber()
+    {
         $this->assertTrue(\ElseifAB\IDKollen\Auth\Validate::socialSecurityNumber('8112189876'));
+    }
+
+    public function testLongSocialSecurityNumber()
+    {
         $this->assertTrue(\ElseifAB\IDKollen\Auth\Validate::socialSecurityNumber('198112189876'));
     }
 
